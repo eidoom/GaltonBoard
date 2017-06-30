a31b12c79ab49ba6db64d9888381fe29cfed5012
David Grellscheid <d.grellscheid@gmail.com>
2017-06-30 08:39:10 +0100

    datareader done

--- galton/datareader.py
+++ galton/datareader.py
@@ -3,12 +3,15 @@ import urllib2
 class BrokenData(Exception):
     pass
 
-def read():
+def read_board_data():
     url = 'http://10.10.10.10/galton_counts.txt'
 
-    data = urllib2.urlopen(url)
-    counts = data.readline()
-    data.close()
+    try:
+        conn = urllib2.urlopen(url, timeout=3)
+        counts = conn.readline()
+        conn.close()
+    except:
+        return [0]*13
 
     counts = counts.rstrip().split(',')
     if counts[0] != 'a' or counts[-1] != 'z':
@@ -23,5 +26,5 @@ def read():
     return counts
 
 if __name__ == '__main__':
-    data = read()
+    data = read_board_data()
     print data
